a1dfee0acfd2a3a9d8cf9847a0b8b83dfcafe0e6
18lily2018 <aggneswang@gmail.com>
2025-12-02 12:29:19 -0500

    Assignment 2 complete: add aisd_vision, aisd_motion, aisd_msgs

--- run_motion.sh
+++ run_motion.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-# Launcher for motion node (Terminal A)
-
-# Load ROS2 + workspace
-source ~/.ros2_auto_setup.sh
-
-# Go to workspace (just in case)
-A
-
-cd ~/ros2_ws_clean
-
-# Run motion node
+source /opt/ros/humble/setup.bash
+source install/setup.bash
+echo "TurtleSim Motion Started"
 ros2 run aisd_motion move
-
